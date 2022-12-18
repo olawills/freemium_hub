be92a926f4599fe6deb2a77dfec2ab8651411793
olawills <wobdele@gmail.com>
2022-12-18 13:32:49 +0100

    fremium_hub starter

---
+++ lib/styles/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// Dark Colors
+class DarkThemeColors {
+  static Color darkThemeAppbar = const Color(0xff5b6774);
+  static Color drawerDarkTheme = const Color(0xff2d2c36);
+  static Color textColors = const Color(0xffffffff);
+  static Color floatingActionButtonColor = const Color(0xff34284e);
+}
+
+// Light Colors
+class LightThemeColors {
+  static Color lightThemeAppbar = const Color.fromARGB(255, 243, 241, 241);
+  static Color drawerDarkTheme = const Color.fromARGB(207, 243, 235, 235);
+  static Color textColors = const Color.fromARGB(255, 32, 33, 34);
+  static Color floatingActionButtonColor = const Color(0xff34284e);
+}
