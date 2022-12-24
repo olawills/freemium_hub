f3ea3eefc0519626072ce7f04e3781b75f2f8413
olawills <wobdele@gmail.com>
2022-12-24 14:47:05 +0100

    Building the UI

---
+++ lib/widgets/terms_and_privacy_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:freemium_hub/widgets/widget_extensions.dart';
+
+
+class TermsAndPrivacyWidget extends StatelessWidget {
+  const TermsAndPrivacyWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'By signing up you agree to our',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        SizedBox(height: 10..spacingH),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {},
+              child: const Text(
+                'Terms of use',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            SizedBox(width: 10..spacingW),
+            const Text('and'),
+            SizedBox(width: 10..spacingW),
+            InkWell(
+              onTap: () {},
+              child: const Text(
+                'privacy policy',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
